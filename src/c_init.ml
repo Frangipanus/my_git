fc952c5005f1562f4b41de08ea768f49bf51af34
Paul <pjtraphael@gmail.com>
2025-02-23 15:49:28 +0100

    init done

--- src/c_init.ml
+++ src/c_init.ml
@@ -5,7 +5,7 @@ let has_git path =
   let dossier = Unix.opendir path in 
   try while true do 
     try let acc = Unix.readdir dossier in 
-      if acc = ".git" then raise Found
+      if acc = ".bite" then raise Found
     with 
       
       |End_of_file -> raise Not_found
@@ -16,4 +16,15 @@ let has_git path =
     |Not_found -> false
 
 let init path = 
-  if has_git path then 
+  if has_git path then Printf.printf "Echec: le répertoire a déja un dossier .git. Le supprimer et retenter.";
+  Unix.mkdir ".bite" 0o777;
+  Unix.mkdir ".bite/objects" 0o777;
+  Unix.mkdir ".bite/refs" 0o777;
+  let _ = Unix.openfile ".bite/HEAD" ([Unix.O_CREAT]) 0o777 in 
+  let _ = Unix.openfile ".bite/config" ([Unix.O_CREAT]) 0o777 in 
+  let _ = Unix.openfile ".bite/description" ([Unix.O_CREAT]) 0o777 in 
+  ()
+
+let () = init "."
+
+  
