b8946a80c73397079d9c2e82a8cbcde23badbf1d
JeanGalte <wontusemailcom@protonmail.com>
2025-05-09 17:57:23 +0200

    biteignore au bon endroit

--- src/c_init.ml
+++ src/c_init.ml
@@ -8,12 +8,12 @@ let c_init (path : string) : unit =
   if has_bite path then
     (Printf.eprintf "Echec: %s contient déja un dossier .bite" path; exit 1)
   else
-    let _ = openfile ".biteignore" [O_CREAT] 0o770 in 
     mkdir ".bite" 0o770;
     mkdir ".bite/objects" 0o770;
     mkdir ".bite/refs" 0o770;
     mkdir ".bite/branches" 0o770;
     mkdir ".bite/branches/main" 0o770;
+    let _ = openfile ".bite/biteignore" [O_CREAT] 0o770 in 
     let _ = openfile ".bite/HEAD" [O_CREAT] 0o770 in
     let _ = openfile ".bite/config" [O_CREAT] 0o770 in
     let _ = openfile ".bite/description" [O_CREAT] 0o770 in
